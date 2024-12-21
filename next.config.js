afe017c109c0229b244822f335c329480daae2c6
fayolamenezes <fayolamenezes16@gmail.com>
2024-12-21 18:52:37 +0530

    h

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
+// next.config.js
 module.exports = {
     assetPrefix: './', // Adjust asset paths for GitHub Pages
-    basePath: '/<product-comparison>',
+    basePath: '/product-comparison', // Replace with your repo name
+    output: 'export', // Enable static export for the app
   };
   
